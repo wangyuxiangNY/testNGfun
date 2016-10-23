99736adc1d764b789b8c183aaabb2329e3196a9a
yuxiangwangNY <yuxiang.wang.ny@gmail.com>
2016-10-23 17:14:06 -0400

    fix

--- testng.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\azurewangyx\git\testNGfun
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
